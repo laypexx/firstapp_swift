35d0ba3f8a12e34c400b7d4ab7d6faf91a579589
laypexx <102018524+laypexx@users.noreply.github.com>
2022-09-09 14:18:44 +0200

    adding app

---
+++ firstapp/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  firstapp
+//
+//  Created by Dominic Sprenger on 09.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Button(action: {
+                print("MyNewPrimitiveButton triggered. Is it printed ?")
+            }){ Text("longpress Button").padding() }
+                .buttonStyle(MyNewPrimitiveButtonStyle(color: .blue))
+
+        }
+    }
+}
+ 
+struct MyNewPrimitiveButtonStyle: PrimitiveButtonStyle {
+    var color: Color
+
+    func makeBody(configuration: PrimitiveButtonStyle.Configuration) -> some View {
+        MyButton(configuration: configuration, color: color)
+    }
+   
+    struct MyButton: View {
+        @State private var pressed = false
+
+        let configuration: PrimitiveButtonStyle.Configuration
+        let color: Color
+
+        var body: some View {
+
+            return configuration.label
+                .foregroundColor(.black)
+                .padding(15)
+                .background(RoundedRectangle(cornerRadius: 5).fill(color))
+                .compositingGroup()
+                .shadow(color: .black, radius: 3)
+                .opacity(self.pressed ? 0.5 : 1.0)
+                .scaleEffect(self.pressed ? 0.8 : 1.0)
+                .onLongPressGesture(minimumDuration: 2.5, maximumDistance: .infinity, pressing: { pressing in
+                withAnimation(.easeInOut(duration: 1.0)) {
+                    self.pressed = pressing
+                }
+                if pressing {
+                    print("My long pressed starts")
+                    print("     I can initiate any action on start")
+                } else {
+                    print("My long pressed ends")
+                    print("     I can initiate any action on end")
+                }
+            }, perform: { })
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ firstapp/firstappApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstappApp.swift
+//  firstapp
+//
+//  Created by Dominic Sprenger on 09.09.22.
+//
+
+import SwiftUI
+
+@main
+struct firstappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
